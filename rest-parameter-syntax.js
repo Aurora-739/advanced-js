b19d82fe48eec06e7e595d7a121ff95a68773230
Aurora-739 <yfifej9y@students.codeinstitute.net>
2025-05-07 09:31:49 +0100

    complete part 1 & 2)

---
+++ rest-parameter-syntax.js
@@ -0,0 +1,13 @@
+/**
+ * To run this file in Gitpod, use the 
+ * command node rest-parameter-syntax.js in the terminal
+ */
+
+// Regular function call 
+const sumAll = (a,b, c) => a + b + c;
+let sum = sumAll(1, 2, 3);
+console.log("sum", sum);
+
+// Extra arguments are ignored
+
+// Function using ...rest
